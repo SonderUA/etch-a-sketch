3a950ffbe45474f8e82c312eb64b526303ab9465
SonderUA <umarbek.sonder@gmail.com>
2024-10-17 19:05:34 +0500

    feat: recreate the loop with minor changes

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -11,4 +11,26 @@ button.addEventListener("click", () => {
 		size = +prompt("Enter the number of squares per side: ");
 		console.log(size <= 100 && size > 0 && Number.isInteger(size));
 	} while (!(size <= 100 && size > 0 && Number.isInteger(size)));
+
+    container.innerHTML = "";
+	for (let i = 0; i < size * size; i++) {
+		let opacity = 0;
+		let backgroundColor;
+		const div = document.createElement("div");
+		div.setAttribute(
+			"style",
+			`width: ${100 / size}%; 
+            aspect-ratio: 1 / 1;`
+		);
+		container.appendChild(div);
+		div.addEventListener("mouseover", () => {
+			if (!backgroundColor) {
+				backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+				div.style.backgroundColor = backgroundColor;
+			}
+			if (opacity < 10) {
+				div.style.opacity = `${++opacity / 10}`;
+			}
+		});
+	}
 });
